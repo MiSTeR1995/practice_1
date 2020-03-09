f981380c318389fc04b5e6b28b0080f26efd4afc
Alex <36799721+MiSTeR1995@users.noreply.github.com>
2020-03-09 13:46:39 +0300

    09_03_2020

--- js/site.js
+++ js/site.js
@@ -1,12 +1,29 @@
+let arr  = [];
+let minArrayValue = -100;
+let maxArrayValue = 100;
 
-let a1 = 20;
-let a2 = 21;
-let a0 = 8;
-
-
-if (a2 <= a1 && a2>=a0) {
-    console.log("Опа привет!");
+for (let i = 0; i < 10; i++) {
+    arr[i] = Math.floor(Math.random() * (maxArrayValue - minArrayValue + 1) + minArrayValue);
 }
-else {
-    console.log("давай до свидания");
+
+console.log('Первоначальный массив: [' + arr + "]");
+
+let indMin = 0;
+let indMax = 0;
+
+for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] < arr[indMin]) {
+        indMin = i;
+    }
+    if (arr[i] > arr[indMax]) {
+        indMax = i;
+    }
 }
+console.log(arr[indMin], arr[indMax]);
+
+let temp = arr[indMin];
+arr[indMin] = arr[indMax];
+arr[indMax] = temp;
+
+console.log("Измененный массив: [" + arr + "]");
